91710a8f2fadf5bbd318781693d501483b1bfa70
mdreece <87138240+mdreece@users.noreply.github.com>
2023-04-01 06:48:44 -0400

    Update chat_gpt3_addon_INSTALL.bat

--- chat_gpt3_addon_INSTALL.bat
+++ chat_gpt3_addon_INSTALL.bat
@@ -1,57 +1,86 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set "BLENDER_EXE_PATH="
+
 for /L %%x in (10, -1, 1) do (
     for /L %%y in (9, -1, 0) do (
-        set "BLENDER_PATH=C:\Program Files\Blender Foundation\Blender %%x.%%y\%%x.%%y\python\bin\python.exe"
-        if exist !BLENDER_PATH! (
-            set "BLENDER_VERSION=%%x.%%y"
-            echo Found Python.exe at: !BLENDER_PATH!
-            cd "C:\Program Files\Blender Foundation\Blender %%x.%%y\%%x.%%y\python\bin"
-            python.exe -m pip install openai
-            goto :done
+        for /L %%z in (9, -1, 0) do (
+            set "BLENDER_PATH=C:\Program Files\Blender Foundation\Blender %%x.%%y\%%x.%%y\python\bin\python.exe"
+            if exist !BLENDER_PATH! (
+                set "BLENDER_VERSION=%%x.%%y"
+                set "BLENDER_EXE_PATH=C:\Program Files\Blender Foundation\Blender %%x.%%y\blender.exe"
+                goto :install
+            ) else (
+                set "BLENDER_PATH=C:\Program Files\Blender Foundation\Blender %%x.%%y.%%z\%%x.%%y\python\bin\python.exe"
+                if exist !BLENDER_PATH! (
+                    set "BLENDER_VERSION=%%x.%%y.%%z"
+                    set "BLENDER_EXE_PATH=C:\Program Files\Blender Foundation\Blender %%x.%%y.%%z\blender.exe"
+                    goto :install
+                )
+            )
         )
     )
 )
 
 echo Python.exe not found.
 exit /b 1
-cls
-:done
+
+:install
+echo Found Python.exe at: !BLENDER_PATH!
+cd /d "!BLENDER_PATH!\.."
+python.exe -m pip install openai
+
+
 echo Installation completed.
 
-:: Check if curl is installed
-where curl >nul 2>nul
-if %errorlevel% neq 0 (
-    echo curl not found, downloading and installing curl...
 
-    :: Download curl
-    set curl_url=https://curl.se/windows/dl-7.79.1/curl-7.79.1-win64-mingw.zip
-    set curl_zip=curl.zip
 
-    powershell -Command "& { (New-Object System.Net.WebClient).DownloadFile('%curl_url%', '%curl_zip%') }"
 
-    :: Extract curl
-    set curl_folder=curl
-    powershell -Command "& { Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('%curl_zip%', '%curl_folder%') }"
 
-    :: Add curl to PATH
-    set PATH=%cd%\%curl_folder%\bin;%PATH%
+:: Download curl
+set curl_url=https://curl.se/windows/latest.cgi?p=win64-mingw.zip
+set curl_zip=%userprofile%\Documents\curl.zip
 
-    echo curl installed.
+powershell -Command "& { Invoke-WebRequest -Uri '%curl_url%' -OutFile '%curl_zip%' }"
+
+:: Extract curl
+set curl_folder=%userprofile%\Documents\curl
+powershell -Command "& { Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('%curl_zip%', '%curl_folder%') }"
+
+:: Add curl to PATH
+set PATH=%curl_folder%\bin;%PATH%
+
+echo curl installed.
 )
+
+CLS
+
 
 :: Download file
 set url=https://raw.githubusercontent.com/mdreece/ChatPGT3-Blender_Addon/main/chat_gpt3_addon.py
-set output_file=%appdata%\Blender Foundation\Blender\!BLENDER_VERSION!\scripts\addons\chat_gpt3_addon.py
 
-@echo off
-curl -L -o "%output_file%" %url%
+:: Extract major and minor version numbers
+for /F "tokens=1,2 delims=." %%a in ("!BLENDER_VERSION!") do (
+    set "major_version=%%a"
+    set "minor_version=%%b"
+)
+
+:: Combine major and minor version numbers to form the correct path
+set "addon_folder=%appdata%\Blender Foundation\Blender\!major_version!.!minor_version!\scripts\addons"
+set "output_file=!addon_folder!\chat_gpt3_addon.py"
+
+:: Ensure addon folder exists
+if not exist "!addon_folder!" (
+    mkdir "!addon_folder!"
+)
+
+curl -L -o "!output_file!" %url%
 cls
 echo :: INSTALLATION OF CHAT_GPT3_ADDON COMPLETE ::
-echo		- OPENAI PYTHON LIBRARY 
-echo		- CURL 
-echo		- CHAT_GPT3_ADDON (V0.9.2)
+echo        - OPENAI PYTHON LIBRARY 
+echo        - CURL 
+echo        - CHAT_GPT3_ADDON (V0.9.2)
 echo.
 echo :: BLENDER WILL OPEN WHEN HITTING ENTER ::
 echo ::  - ENABLE ADDON                      ::
@@ -65,8 +94,11 @@ pause >nul
 cls
 
 :: Automatically enable the addon
-
-start "" "C:\Program Files\Blender Foundation\Blender !BLENDER_VERSION!\blender.exe" --python-use-system-env --addons chat_gpt3_addon
+if not "!BLENDER_EXE_PATH!"=="" (
+    start "" "!BLENDER_EXE_PATH!" --python-use-system-env --addons chat_gpt3_addon
+) else (
+    echo Blender.exe not found.
+)
 
 TIMEOUT /T 3 >NUL
 CLS
